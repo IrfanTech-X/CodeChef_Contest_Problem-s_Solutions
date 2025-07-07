d479a6bd2aa25491d931fe55fecd845144de9433
Irfan <siamtalukdar3@gmail.com>
2025-07-07 21:15:06 +0600

    practice!

---
+++ JUSTICE.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sn=new Scanner(System.in);
+		int X=sn.nextInt();
+		int Y=sn.nextInt();
+		if(X>=Y){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+
+	}
+}
---
+++ YOGADAY.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sn=new Scanner(System.in);
+		int n=sn.nextInt();
+		int r=(n/12);
+		System.out.println(r);
+
+	}
+}
