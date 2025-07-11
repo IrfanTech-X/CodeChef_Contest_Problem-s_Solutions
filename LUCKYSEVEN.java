824a5ee02c8ebda6a689faf75f5e040f38c67651
Irfan <siamtalukdar3@gmail.com>
2025-07-11 19:38:26 +0600

    practice!

---
+++ LUCKYSEVEN.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sn=new Scanner(System.in);
+		
+		String a=sn.nextLine();
+		
+		char b=a.charAt(6);
+		System.out.println(b);
+
+	}
+}
