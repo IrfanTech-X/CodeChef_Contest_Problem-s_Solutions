9d3f46daf876fe2b6d0a8e09b49a8452ce828181
Irfan <siamtalukdar3@gmail.com>
2025-10-07 02:18:03 +0600

    Practice

---
+++ CHRISTGREET.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sn=new Scanner(System.in);
+		int a=sn.nextInt();
+		if(a==25){
+		    System.out.println("Christmas");
+		}else{
+		    System.out.println("Ordinary");
+		}
+
+	}
+}
